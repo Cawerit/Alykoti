6fbd79d599453b11bb231a91f3334ef13b477db5
noppa <the.nobba@gmail.com>
2016-03-11 20:53:47 +0200

    merge

---
+++ src/com/example/alykoti/AlykotiUI.java
@@ -0,0 +1,39 @@
+package com.example.alykoti;
+import javax.servlet.annotation.WebServlet;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+
+@SuppressWarnings("serial")
+@Theme("alykoti")
+public class AlykotiUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = AlykotiUI.class)
+    public static class Servlet extends VaadinServlet {
+
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
+
+        Button button = new Button("Kaynnista peskone");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+                layout.addComponent(new Label("Hahaha"));
+            }
+        });
+        layout.addComponent(button);
+    }
+
+}
