e3fdea24f06aaadfa6e850d7a76c56eeb3208293
Timo Heinonen <jootimo@gmail.com>
2016-03-29 16:58:44 +0300

    new branch

--- src/com/example/alykoti/HomeView.java
+++ src/com/example/alykoti/HomeView.java
@@ -8,9 +8,15 @@ import com.sun.istack.internal.Nullable;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Accordion;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.TreeTable;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -18,32 +24,54 @@ import java.util.List;
 public class HomeView extends AppView {
 
     public final Accordion accordion = new Accordion();
-	public final Label homeLabel = new Label();
-
+	//public final Label homeLabel = new Label();
+	public final VerticalLayout content = new VerticalLayout();
+	public final Panel homePanel = new Panel();
+	public final Button roomButton = new Button();
+	
     public HomeView(){
 		super(AuthService.Role.USER);
-		addComponent(homeLabel);
-		addComponent(accordion);
+		//addComponent(homeLabel);
+		content.setSizeFull();
+		homePanel.setSizeUndefined();
+		homePanel.setContent(accordion);
+		content.addComponent(homePanel);
+		content.addComponent(roomButton);
+		content.setComponentAlignment(homePanel, Alignment.MIDDLE_CENTER);
+		content.setComponentAlignment(roomButton, Alignment.TOP_CENTER);
+		addComponent(content);
     }
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event){
         super.enter(event);
 		Integer homeId = Integer.parseInt(event.getParameters());//Kodin id saadaan url parametreista
-		//Tyhj√§t√§√§n vanha sis√§lt√∂
+		//Tyhj‰t‰‰n vanha sis‰ltˆ
 		accordion.removeAllComponents();
-		//Luodaan ylin palkki, joka sis√§lt√§√§ napin huoneen lis√§√§miseen
+		//Luodaan ylin palkki, joka sis‰lt‰‰ napin huoneen lis‰‰miseen
 		Room newRoom = new Room();
 		newRoom.setHomeId(homeId);
 		TabSheet.Tab addRoomComponent = accordion.addTab(new RoomComponent(newRoom, (Room room) -> {
 			//Callback lis√§√§ uuden huoneen listaan
 			TabSheet.Tab newTab = accordion.addTab(new RoomComponent(room), room.getName());
-		}), "Lis√§√§ huone", FontAwesome.PLUS);
+		}), "Lis‰‰ huone", FontAwesome.PLUS);
 
+		//Nappi, jolla siirryt‰‰n huonen‰kym‰‰n
+		roomButton.setCaption("Open Room View");
+		roomButton.setIcon(FontAwesome.EYE);
+		roomButton.addClickListener(new ClickListener(){
+			@Override
+			public void buttonClick(ClickEvent event) {
+				//TODO: uri-fragmentilla(?) oikeaan taloon
+				AlykotiUI.NAVIGATOR.navigateTo(AlykotiUI.ROOMVIEW + "/" + homeId); // + "/" + roomId
+			}
+		});
+		
+		
         Home home = new Home(homeId);
 		try {
-			home.pull();//P√§ivitet√§√§n koti-olion sis√§lt√∂ tietokannasta
-			homeLabel.setValue(home.getName());
+			home.pull();//P‰ivitet‰‰n koti-olion sis‰ltˆ tietokannasta
+			homePanel.setCaption(home.getName());
 			List<Room> rooms = home.getRooms();
 			for(Room r : rooms)
 				accordion.addTab(new RoomComponent(r), r.getName(), FontAwesome.HOME);
